434a72e548ac5a34a2a6293bffe55a53540a05cc
SpReeD <wackermann@nexn.systems>
2020-11-17 23:58:41 +0100

    Added Icon - Changed to WindowsApplication

---
+++ Caffeine2/CaffeineEngine.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using EventHook;
+using WindowsInput;
+using WindowsInput.Native;
+
+public class CaffeineEngine : IDisposable
+{
+    #region Config Properties
+    private TimeSpan KeyPressInterval = new TimeSpan(0, 0, 59);
+    #endregion
+    #region Publics
+    public VirtualKeyCode KeyToPress { get; set; } = VirtualKeyCode.F23;
+    public DateTime LastKeyPressEvent { get; set; } = DateTime.Now;
+    #endregion
+
+    #region Privates
+    private readonly KeyboardWatcher keyboardWatcher;
+    private readonly Timer CaffeineTimer = new Timer();
+    private readonly InputSimulator inputSimulator = new InputSimulator();
+    #endregion
+
+    #region Constructor
+    public CaffeineEngine()
+    {
+        keyboardWatcher = new EventHookFactory().GetKeyboardWatcher();
+        keyboardWatcher.Start();
+        keyboardWatcher.OnKeyInput += GlobalKeyEvents;
+
+        CaffeineTimer.Elapsed += CaffeineTimer_Elapsed;
+        CaffeineTimer.Interval = new TimeSpan(0, 0, 1).TotalMilliseconds;
+        CaffeineTimer.Start();
+    }
+    #endregion
+    #region Events
+    private void GlobalKeyEvents(object sender, KeyInputEventArgs e)
+    {
+        LastKeyPressEvent = DateTime.Now;
+        Debug.Print("Keypress: " + e.KeyData.Keyname);
+    }
+
+    private void CaffeineTimer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        if ((DateTime.Now - LastKeyPressEvent).TotalSeconds >= KeyPressInterval.TotalSeconds)
+        {
+            SendKey();
+            LastKeyPressEvent = DateTime.Now;
+        }
+    }
+    #endregion
+
+    private void SendKey()
+    {
+        this.inputSimulator.Keyboard.KeyPress(KeyToPress);
+        Debug.Print("Keypress sent: " + KeyToPress.ToString());
+    }
+
+    public void Dispose()
+    {
+        if (this.keyboardWatcher != null)
+        {
+            keyboardWatcher.Stop();
+        }
+        this.CaffeineTimer.Dispose();
+    }
+}
--- Caffeine2/Program.cs
+++ Caffeine2/Program.cs
@@ -1,62 +1,22 @@
 ﻿using System;
-using System.Reflection;
-using System.Timers;
-using System.Windows.Forms;
 using System.Diagnostics;
-using EventHook;
-using WindowsInput;
-using WindowsInput.Native;
 
 namespace Caffeine2
 {
     class Program
     {
-        #region Config Properties
-        private static TimeSpan KeyPressInterval = new TimeSpan(0,0,11);
-        #endregion
-        public static VirtualKeyCode KeyToPress { get; set; } = VirtualKeyCode.F23;
-        public static DateTime LastKeyPressEvent { get; set; } = DateTime.Now;
-
-        private static KeyboardWatcher keyboardWatcher;
-        private static readonly System.Timers.Timer CaffeineTimer = new System.Timers.Timer();
         static void Main(string[] args)
         {
-            keyboardWatcher = new EventHookFactory().GetKeyboardWatcher();
-            keyboardWatcher.Start();
-            keyboardWatcher.OnKeyInput += GlobalKeyEvents;
+            _ = new CaffeineEngine();
 
-            CaffeineTimer.Elapsed += CaffeineTimer_Elapsed;
-            CaffeineTimer.Interval = new TimeSpan(0,0,1).TotalMilliseconds;
-            CaffeineTimer.Start();
-
-            while (true)
-            {
-                Console.ReadKey();
-            }
-        }
-
-        static void GlobalKeyEvents(object sender, KeyInputEventArgs e)
-        {
-            LastKeyPressEvent = DateTime.Now;
-            Debug.Print("Keypress: " + e.KeyData.Keyname);
-        }
-
-        static void CaffeineTimer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            if ((DateTime.Now - LastKeyPressEvent).TotalSeconds >= KeyPressInterval.TotalSeconds)
-            {
-                SendKey();
-                LastKeyPressEvent = DateTime.Now;
-            }
-        }
-
-        static void SendKey()
-        {
-            string hexStr = "0x" + ((int)KeyToPress).ToString("X"); //Build Hexstring for selected ASCII-Key enum
-            WindowsInput.InputSimulator s = new InputSimulator();
-            s.Keyboard.KeyPress(KeyToPress);
-            Debug.Print("Keypress sent: " + KeyToPress.ToString());
+            System.Windows.Forms.NotifyIcon k = new System.Windows.Forms.NotifyIcon();
             
+
+            //while (true)
+            //{
+            //    Console.ReadKey();
+            //}
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
         }
     }
 }
