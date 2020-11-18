6b759818a2264d86903142f08a9c389ecbd8ac88
SpReeD <wackermann@nexn.systems>
2020-11-18 16:44:48 +0100

    Application.Run()

--- Caffeine2/Program.cs
+++ Caffeine2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Diagnostics;
 
 namespace Caffeine2
@@ -7,14 +8,26 @@ namespace Caffeine2
     {
         static void Main(string[] args)
         {
-            _ = new CaffeineEngine();
+            //_ = new CaffeineEngine();
+
+            System.Windows.Forms.ContextMenuStrip cxt = new System.Windows.Forms.ContextMenuStrip();
+            System.Windows.Forms.ToolStripMenuItem o = new ToolStripMenuItem();
+            o.Text = "Hello";
+            o.Size = new System.Drawing.Size(182,48);
+            cxt.Items.Add(o);
+            
 
             System.Windows.Forms.NotifyIcon k = new System.Windows.Forms.NotifyIcon();
             k.Icon = Properties.Resources.computer_78938;
+            k.ContextMenuStrip = cxt;
+            k.Click += (x,y) => { Debug.Print("Works"); };
             k.Visible = true;
+            k.Text = "Caffeine2";
+            //k.BalloonTipText = "Loaded...";
+            //k.BalloonTipTitle = "Caffeine2";
+            //k.ShowBalloonTip(1);
 
-            System.Windows.Forms.ContextMenu cxt = new System.Windows.Forms.ContextMenu();
-            //TODO: ... :)
+            Application.Run(); // GUI needs to run in another Thread that's inside the main Thread
 
             //while (true)
             //{
